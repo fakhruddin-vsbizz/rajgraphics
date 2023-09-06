c012036f2a0d5140a36f570ec54189130cdceece
fakhruddin-vsbizz <fakhruddinvsbizz@gmail.com>
2023-09-06 13:38:00 +0530

    title changed for the website

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import '@/styles/tailwind.css'
 export const metadata = {
   title: {
     template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    default: 'Raj Graphics - Technical Solutions and Graphics Studio',
   },
 }
 
